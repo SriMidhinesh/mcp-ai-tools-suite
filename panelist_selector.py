4690315281950d993e95069cec436d799c9609b5
SriMidhinesh <midhinesh.92@gmail.com>
2025-07-18 14:55:32 +0530

    change panelist selector toolwith Gemini AI

--- panelist_selector.py
+++ panelist_selector.py
@@ -1,36 +1,50 @@
-import asyncio
 import pandas as pd
-from typing import List
-import openpyxl
+from google import genai
+from dotenv import load_dotenv
+import os
 from fastmcp import FastMCP
-from mcp.server.streamable_http import StreamableHTTPServerTransport
 
-path = "panel_member_info.xlsx"
-mcp = FastMCP("mcp-member-selector")
-df = pd.read_excel(path)
-transport = StreamableHTTPServerTransport(
-    mcp_session_id="panelist_selector"
-)
+mcp = FastMCP("panelist_selector")
+
+load_dotenv()
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+client = genai.Client(api_key = google_api_key)
+
+input_data = {
+    "panel_df" : "pd.read_excel('panel_member_info.xlsx')",
+    "candidate_info" : "Sri Midhinesh Sankurabhukta, with 6 months of experience in Sonata Software whose skills are Python and Machine Learning, is scheduled for an interview on 19th July 2025 at 10:00 AM."
+}
 
 @mcp.tool()
-def member_selector(primary: str, secondary: str) -> List[str]:
+def panelist_selector(panel_info: pd.DataFrame, candidate_info: str) -> str | None:
     """
-    Matches a candidate to the most suitable panelist based on primary and secondary skills using data from an Excel file.
+    Matches a candidate to the most suitable panelist based on Primary, Secondary skills, Availablity and Expereince using data from an Excel file.
     """
-    matched_primary = df[df["Primary Skill"] == primary]["Panel Member"]
-    matched_secondary = df[df["Secondary Skill"] == secondary]["Panel Member"]
+    columns = list(panel_info.columns)
+    panel_text = "\n".join(
+        " | ".join(f"{col}: {row[col]}" for col in columns)
+        for _, row in panel_info.iterrows()
+    )
     
-    matched_panelists = pd.concat([matched_primary, matched_secondary]) \
-                           .drop_duplicates() \
-                           .tolist()
-                           
-    return matched_panelists
+    prompt = f"""
+    
+    This is the Information available on the candidate. {candidate_info}
+    
+    Here is the list of panelists:
+    {panel_text}
+    
+    Based on skill, match and experience, Select the most suitable panelist.
+    Make sure that the interview's experience is always more than the candidate's experience
+    Respond with their name and reason for your choice. Give a sarcastic reason as well.
+    """
+    
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash-lite",
+        contents = prompt
+    )
+    
+    return response.text
 
-app = mcp.streamable_http_app()
- 
-#execution logic on startup
-@app.on_event("startup")
-async def startup():
-    transport.connect()
-    asyncio.create_task(mcp.run_streamable_http_async())
-
+if __name__ == "__main__":
+    mcp.run(transport = "stdio")
